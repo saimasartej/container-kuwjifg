d8026ac3ae0a4448c2abf51994ffdbbae7592a2c
saimasartej <139892532+saimasartej@users.noreply.github.com>
2023-07-27 22:54:47 +0530

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -2,31 +2,22 @@ from flask import Flask, render_template
 from db import get_db, close_db
 import sqlalchemy
 from logger import log
+from requests import Session
+import time
 
+TOKEN = "5955602844:AAFfwmGzOaZoOClIKPtOSLkBjjbVBnpXuGY"
+CHAT_ID = '6093993760'
+SEND_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 app = Flask(__name__)
-app.teardown_appcontext(close_db)
+
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
 
 
-@app.route("/health")
-def health():
-    log.info("Checking /health")
-    db = get_db()
-    health = "BAD"
-    try:
-        result = db.execute("SELECT NOW()")
-        result = result.one()
-        health = "OK"
-        log.info(f"/health reported OK including database connection: {result}")
-    except sqlalchemy.exc.OperationalError as e:
-        msg = f"sqlalchemy.exc.OperationalError: {e}"
-        log.error(msg)
-    except Exception as e:
-        msg = f"Error performing healthcheck: {e}"
-        log.error(msg)
-
-    return health
+    while True:
+        time.sleep(60)
+        requests.post(SEND_URL, json={'chat_id': CHAT_ID, 'text': 'nifty_kjhindex_data'})  
+    
+    return "kkkjghl"
